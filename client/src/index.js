cbbb592fe0d96e99c991aa72c730ea4bd853edda
Lonnie P <allon.paz@gmail.com>
2019-09-02 14:44:27 -0700

    updating repo

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(
-<App />,
-document.getElementById('root')
-);
+ReactDOM.render(<App />,document.getElementById('root'));
+registerServiceWorker();
 
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
