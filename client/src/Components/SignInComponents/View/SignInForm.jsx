2d8b6aefd44a9971debd4f0dfe73783b49e35295
Lonnie P <allon.paz@gmail.com>
2019-09-21 16:32:31 -0700

    updating repo for the first time in a while

--- client/src/Components/SignInComponents/View/SignInForm.jsx
+++ client/src/Components/SignInComponents/View/SignInForm.jsx
@@ -62,7 +62,7 @@ class SignInForm extends Component {
               </div>
 
               <div className="FormField">
-                  <button className="FormField__Button mr-20">Sign In</button> <Link to="/" className="FormField__Link">Create an account</Link>
+                  <button className="FormField__Button mr-20">Sign In</button> <Link to="/main" className="FormField__Link">Create an account</Link>
               </div>
             </form>
           </div>
