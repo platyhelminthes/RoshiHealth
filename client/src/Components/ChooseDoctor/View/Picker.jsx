075547fcdfaf2392dabd7df5df5bcc45956a2d1d
Devon <Dvowen@cox.net>
2019-10-06 20:34:56 -0700

    merging

---
+++ client/src/Components/ChooseDoctor/View/Picker.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import {Component} from 'react'
+
+
+class Picker extends Component {
+
+    constructor(props){
+        super(props)
+        state = {
+            loading: true
+        }
+    }
+
+    render(){
+        return(
+
+            <div className="__square-box">
+                
+            </div>
+
+        )
+    }
+
+}
+
+export default Picker
